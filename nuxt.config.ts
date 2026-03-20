be10e3777091b5fcd2764ee12386e32ba6967f58
slakmal <slakmal@coveragex.com>
2026-03-20 09:45:04 +0530

    Update ESLint configuration and `yarn.lock` with additional dependencies and refinements to align with Antfu presets and Nuxt setup.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2025-07-15',
-    devtools: {enabled: true},
-    modules: [
-        '@nuxt/eslint'
-    ],
-    eslint: {
-        // options here
-    }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/eslint",
+  ],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+});
